3d97c3da5dd70408eb36cdc134faf75e72809759
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-12-20 12:43:36 +0300

    Add DataSet function for reading users and phones from json files

--- dataset.go
+++ dataset.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func openDataset() (users []User, phones map[string]string, err error) {
+	usersBytes, err := os.ReadFile("users.json")
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot read users: %w", err)
+	}
+
+	usersPhones, err := os.ReadFile("phones.json")
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot read phones: %w", err)
+	}
+
+	err = json.Unmarshal(usersBytes, &users)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot umarshal users: %w", err)
+	}
+
+	phones = map[string]string{}
+	err = json.Unmarshal(usersPhones, &phones)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot unmarshal phones: %w", err)
+	}
+
+	return users, phones, nil
+}
